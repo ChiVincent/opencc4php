24d4d40c901e2afb34baaa783105c842374f980e
Vincent Chi <song374561@chivincent.net>
2019-02-20 11:43:52 +0800

    Example file update.

--- opencc.php
+++ opencc.php
@@ -1,19 +1,20 @@
 <?php
-$br = (php_sapi_name() == "cli")? "":"<br>";
+$newLine = (php_sapi_name() == 'cli')
+    ? "\n"
+    :'br';
 
 if(!extension_loaded('opencc')) {
 	dl('opencc.' . PHP_SHLIB_SUFFIX);
 }
-$module = 'opencc';
-$functions = get_extension_funcs($module);
-echo "Functions available in the test extension:$br\n";
+
+$functions = get_extension_funcs('opencc');
+echo "Functions available in the test extension: $newLine";
 foreach($functions as $func) {
-    echo $func."$br\n";
+    echo $func . $newLine;
 }
-echo "$br\n";
+echo $newLine;
 
 $od = opencc_open('s2twp.json');
-echo opencc_convert("我的鼠标哪儿去了？", $od);
-echo opencc_error()."\n";
+echo opencc_convert("我的鼠标哪儿去了？", $od) . $newLine;
+echo opencc_error() . $newLine;
 var_dump(opencc_close($od));
-?>
